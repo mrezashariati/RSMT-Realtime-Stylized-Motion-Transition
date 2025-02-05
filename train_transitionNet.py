f0dafbdf21af6f44e3d1819b67c3ecaed6a8bfb6
Mreza <mohammadreza.shariati@cafebazaar.ir>
2025-02-05 16:50:55 +0100

    experimenting

--- train_transitionNet.py
+++ train_transitionNet.py
@@ -246,8 +246,8 @@ def training_style100_phase():
 
         key = "Flapping"
         sty_key = "Flapping"
-        cid = 0
-        sid = 0
+        cid = 42
+        sid = 10
         print(
             "len(app.data_module.test_set.dataset[key]):",
             len(app.data_module.test_set.dataset[key]),
